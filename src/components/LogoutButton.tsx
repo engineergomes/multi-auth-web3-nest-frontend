3b94ddc9d00e85431de7de6058d28ae265c2fa71
Rafael <eng.rsgomes@gmail.com>
2023-10-05 23:16:11 -0300

    add logout

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -3,35 +3,25 @@ import React, { useState, forwardRef, HTMLAttributes } from "react";
 import LogoutModal from "./LogoutModal";
 import { useProfile, useLogout } from "@/hooks/onUser";
 
-interface logoutButtonProps extends HTMLAttributes<HTMLDivElement> {}
+export default function LogoutButton() {
+	const userProfile = useProfile();
+	const logout = useLogout();
+	const [isOpen, setIsOpen] = useState(false);
 
-const LogoutButton = forwardRef<HTMLDivElement, logoutButtonProps>(
-	(props, ref) => {
-		const userProfile = useProfile();
-		const logout = useLogout();
-		const [isOpen, setIsOpen] = useState(false);
-
-		async function logoutHandler() {
-			await logout.refetch();
-			await userProfile.refetch();
-			setIsOpen(!isOpen);
-		}
-
-		return (
-			<>
-				<div {...props} onClick={() => setIsOpen(!isOpen)} ref={ref}>
-					{props.children}
-				</div>
-				<LogoutModal
-					isOpen={isOpen}
-					onLogout={() => logoutHandler()}
-					setIsOpen={() => setIsOpen(!isOpen)}
-				/>
-			</>
-		);
+	async function logoutHandler() {
+		await logout.refetch();
+		await userProfile.refetch();
+		setIsOpen(!isOpen);
 	}
-);
 
-LogoutButton.displayName = "LogoutButton";
-
-export default LogoutButton;
+	return (
+		<>
+			<button onClick={() => setIsOpen(!isOpen)}>Logout</button>
+			<LogoutModal
+				isOpen={isOpen}
+				onLogout={() => logoutHandler()}
+				setIsOpen={() => setIsOpen(!isOpen)}
+			/>
+		</>
+	);
+}
